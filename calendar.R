a54a4aae4771c25bd2b512794d9761311035b998
Toniiiio <=>
2022-01-11 21:23:28 +0100

    repaired month date render

--- calendar.R
+++ calendar.R
@@ -71,17 +71,26 @@ data <- data.frame(
 data <- block_template_vo2_1
 
 js_current_date <- "
+
+var elements = document.getElementsByClassName('tui-full-calendar-weekday-grid-line  tui-full-calendar-near-month-day');
+for (var i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('click', function(el) {
+    var target = el.target;
+    Shiny.onInputChange('clicked_data', {val: target.textContent, rand: Math.random()});
+  });
+}
+
+
 var elements = document.getElementsByClassName('tui-full-calendar-month-week-item');
 for (var i = 0; i < elements.length; i++) {
   elements[i].addEventListener('click', function(el) {
     var target = el.target;
-    Shiny.onInputChange('val', target.textContent)
     while (target.className != 'tui-full-calendar-month-week-item') {
       target = target.parentElement
     }
     Shiny.onInputChange('row_idx', [...target.parentElement.children].indexOf(target))
   });
-    }
+}
 "
 
 
@@ -98,17 +107,6 @@ server <- function(input, output, session) {
     shinyjs::runjs(js_current_date)
   }, once = FALSE )
   
-  
-  # session$onFlushed(function() shinyjs::runjs("
-  # var elements = document.getElementById('my_calendar').getElementsByTagName('*');
-  # for(var nr = 0, len = elements.length; nr < len; nr++) {
-  #   if(elements[nr].classList.contains('tui-full-calendar-weekday-grid-line')){
-  #     elements[nr].onclick = function (e) {
-  #       Shiny.onInputChange('clicked_data', {val: e.target.textContent, rand: Math.random()});
-  #     }
-  #   }
-  # }
-  # "), once = FALSE)
   
   observe({
     
@@ -173,13 +171,14 @@ server <- function(input, output, session) {
       location = NA,
       bgColor = NA,
       color = NA,
-      borderColor = NA
+      borderColor = NA,
+      date_diff = NA
     )
     
     
     add$title <- input$title #paste(input$sport_type, input$duration, "min")
-    add$start <- paste0("2022-01-", rep(0, 2 - nchar(global$click_date)), global$click_date)
-    add$end <- paste0("2022-01-", rep(0, 2 - nchar(global$click_date)), global$click_date)
+    add$start <- paste0("2022-01-", rep(0, 2 - nchar(global$click_date)), global$click_date) %>% as.Date
+    add$end <- paste0("2022-01-", rep(0, 2 - nchar(global$click_date)), global$click_date) %>% as.Date
     isolate(global$data <- rbind(add, global$data))
     print(global$data %>% dput)
   })
@@ -195,7 +194,7 @@ server <- function(input, output, session) {
     current_month <- format(as.Date(input$my_calendar_dates$current),"%m")
     end_month <- format(as.Date(input$my_calendar_dates$end),"%m")
     row_nr = input$row_idx
-    val = gsub(input$val, pattern = "\n| ", replacement = "") %>% as.numeric
+    val = gsub(input$clicked_data$val, pattern = "\n| ", replacement = "") %>% as.numeric
     req(!is.na(val))
     if(row_nr %in% c(0, 1) & val > 16){
       month = start_month
